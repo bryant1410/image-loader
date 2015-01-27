81e1fba725da068b3bd562d9cb6865cc44aa242e
Friedger Müffke <friedger@gmail.com>
2015-01-27 16:50:33 +0100

    fixes #189

--- core/src/main/java/com/novoda/pxfetcher/FileNameFactory.java
+++ core/src/main/java/com/novoda/pxfetcher/FileNameFactory.java
@@ -4,10 +4,4 @@ public interface FileNameFactory<T> {
 
     String getFileName(String sourceUrl, T metadata);
 
-    T extractMetadata(String fileName);
-
-    boolean hasMetadata(String file);
-
-    String getCacheDirectory();
-
 }
