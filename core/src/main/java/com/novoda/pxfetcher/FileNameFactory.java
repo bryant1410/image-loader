a6495ead79c7170a72e3f138245ca91470ce747b
Friedger Müffke <friedger@gmail.com>
2015-01-28 14:48:54 +0100

    add comment about the purpose of the FileNameFactory

--- core/src/main/java/com/novoda/pxfetcher/FileNameFactory.java
+++ core/src/main/java/com/novoda/pxfetcher/FileNameFactory.java
@@ -1,5 +1,9 @@
 package com.novoda.pxfetcher;
 
+/**
+ * A FileNameFactory is responsible to create a file name for a retrieved resource.
+ * @param <T> type of meta data
+ */
 public interface FileNameFactory<T> {
 
     String getFileName(String sourceUrl, T metadata);
