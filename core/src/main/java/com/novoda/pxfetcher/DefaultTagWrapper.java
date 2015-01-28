f775a59030c90619144ae9695f15b4d22e870650
Friedger Müffke <friedger@gmail.com>
2015-01-28 23:18:25 +0100

    add type to DefaultTagWrapper

--- core/src/main/java/com/novoda/pxfetcher/DefaultTagWrapper.java
+++ core/src/main/java/com/novoda/pxfetcher/DefaultTagWrapper.java
@@ -2,15 +2,22 @@ package com.novoda.pxfetcher;
 
 import com.novoda.pxfetcher.task.TagWrapper;
 
-public class DefaultTagWrapper extends TagWrapper<Void> {
+public class DefaultTagWrapper<MetadataType> extends TagWrapper<MetadataType> {
+
+    private final MetadataType metadata;
 
     public DefaultTagWrapper(String url) {
+        this(url, null);
+    }
+
+    public DefaultTagWrapper(String url, MetadataType metadata) {
         super(url);
+        this.metadata = metadata;
     }
 
     @Override
-    public Void getMetadata() {
-        return null;
+    public MetadataType getMetadata() {
+        return metadata;
     }
 
 }
